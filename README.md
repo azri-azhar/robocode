dcb4631beb90bdc0aa5ccb27152fdf3f46dbce8e
Mohamad Azri Azhar <m.azri.azhar@gmail.com>
2017-06-17 18:28:14 +0800

    Initial implementation of a simple Robocode robot.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# robocode
-A simple implementation of Robocode robot. Robocode is a programming tank game (http://robocode.sourceforge.net/).
+# Robocode
+Sandbox is a personal workspace to try out new ideas and store reusable Java code snippets. Sandbox components are not necessarily be maintained, or used in actual production.
+
+# What's Available?
+* SimpleRobot
+
+  A simple implementation of Robocode robot, based on the information gathered in RoboWiki and http://mark.random-article.com/weber/java/robocode/. Robocode is a programming tank game (http://robocode.sourceforge.net/).
---
+++ src/main/java/com/azriazhar/robocode/SimpleRobot.java
@@ -0,0 +1,67 @@
+package com.azriazhar.robocode;
+
+import java.awt.Color;
+
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
+
+/**
+ * Scan for a robot, fire and travel to its last location if detected. Fire and
+ * ram on hit. This robot uses a basic gun and radar movements.
+ *
+ * @author Mohamad Azri Azhar
+ */
+public class SimpleRobot extends Robot {
+
+	@Override
+	public void run() {
+		paintRobot();
+		setAdjustGunForRobotTurn(true);
+		for (;;) {
+			turnGunRight(1000);
+		}
+	}
+
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+		double degrees = Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
+		turnGunRight(degrees);
+
+		fire(Rules.MAX_BULLET_POWER * 10 / e.getDistance());
+		turnRight(e.getBearing());
+		ahead(e.getDistance());
+	}
+
+	@Override
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(90 - e.getBearing());
+		ahead(100);
+	}
+
+	@Override
+	public void onHitWall(HitWallEvent e) {
+		turnRight(90 - e.getBearing());
+		ahead(100);
+	}
+
+	@Override
+	public void onHitRobot(HitRobotEvent e) {
+		double degrees = Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
+		turnGunRight(degrees);
+
+		fire(Rules.MAX_BULLET_POWER);
+		ahead(100);
+	}
+
+	private void paintRobot() {
+		setBodyColor(Color.BLACK);
+		setRadarColor(Color.YELLOW);
+		setGunColor(Color.RED);
+		setBulletColor(Color.RED);
+	}
+}
