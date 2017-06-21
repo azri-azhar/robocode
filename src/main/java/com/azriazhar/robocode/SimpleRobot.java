f8248aeddd960e81add5d4d6d15d3b5feb5d3b10
Mohamad Azri Azhar <m.azri.azhar@gmail.com>
2017-06-22 01:37:25 +0800

    Change source code style to Google Java Style.

--- src/main/java/com/azriazhar/robocode/SimpleRobot.java
+++ src/main/java/com/azriazhar/robocode/SimpleRobot.java
@@ -11,57 +11,60 @@ import robocode.ScannedRobotEvent;
 import robocode.util.Utils;
 
 /**
- * Scan for a robot, fire and travel to its last location if detected. Fire and
- * ram on hit. This robot uses a basic gun and radar movements.
+ * Scan for a robot, fire and travel to its last location if detected. Fire and ram on hit. This
+ * robot uses a basic gun and radar movements.
  *
  * @author Mohamad Azri Azhar
  */
 public class SimpleRobot extends Robot {
 
-	@Override
-	public void run() {
-		paintRobot();
-		setAdjustGunForRobotTurn(true);
-		for (;;) {
-			turnGunRight(1000);
-		}
-	}
+  @Override
+  public void run() {
+    paintRobot();
+    setAdjustGunForRobotTurn(true);
 
-	@Override
-	public void onScannedRobot(ScannedRobotEvent e) {
-		double degrees = Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
-		turnGunRight(degrees);
+    for (;;) {
+      turnGunRight(1000);
+    }
+  }
 
-		fire(Rules.MAX_BULLET_POWER * 10 / e.getDistance());
-		turnRight(e.getBearing());
-		ahead(e.getDistance());
-	}
+  @Override
+  public void onScannedRobot(ScannedRobotEvent e) {
+    double degrees =
+        Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
 
-	@Override
-	public void onHitByBullet(HitByBulletEvent e) {
-		turnRight(90 - e.getBearing());
-		ahead(100);
-	}
+    turnGunRight(degrees);
+    fire(Rules.MAX_BULLET_POWER * 10 / e.getDistance());
+    turnRight(e.getBearing());
+    ahead(e.getDistance());
+  }
 
-	@Override
-	public void onHitWall(HitWallEvent e) {
-		turnRight(90 - e.getBearing());
-		ahead(100);
-	}
+  @Override
+  public void onHitByBullet(HitByBulletEvent e) {
+    turnRight(90 - e.getBearing());
+    ahead(100);
+  }
 
-	@Override
-	public void onHitRobot(HitRobotEvent e) {
-		double degrees = Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
-		turnGunRight(degrees);
+  @Override
+  public void onHitWall(HitWallEvent e) {
+    turnRight(90 - e.getBearing());
+    ahead(100);
+  }
 
-		fire(Rules.MAX_BULLET_POWER);
-		ahead(100);
-	}
+  @Override
+  public void onHitRobot(HitRobotEvent e) {
+    double degrees =
+        Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + e.getBearing());
 
-	private void paintRobot() {
-		setBodyColor(Color.BLACK);
-		setRadarColor(Color.YELLOW);
-		setGunColor(Color.RED);
-		setBulletColor(Color.RED);
-	}
+    turnGunRight(degrees);
+    fire(Rules.MAX_BULLET_POWER);
+    ahead(100);
+  }
+
+  private void paintRobot() {
+    setBodyColor(Color.BLACK);
+    setRadarColor(Color.YELLOW);
+    setGunColor(Color.RED);
+    setBulletColor(Color.RED);
+  }
 }
